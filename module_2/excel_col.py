dc522f3a0db0b880af1ad8164e6631fce035815b
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-10-19 14:38:07 -0700

    started this exercise

---
+++ module_2/excel_col.py
@@ -0,0 +1,14 @@
+"""
+Prompt
+Given a column title as appears in an Excel sheet, return its corresponding column number.
+
+Examples:
+A -> 1    
+B -> 2    
+C -> 3   
+    ...    
+Z -> 26    
+AA -> 27    
+AB -> 28 
+"""
+
