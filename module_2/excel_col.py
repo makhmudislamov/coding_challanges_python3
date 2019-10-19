94efe814a22a7bf3e84068008dfeb5ce7002ade2
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-10-19 14:53:17 -0700

    pseudocode

--- module_2/excel_col.py
+++ module_2/excel_col.py
@@ -12,3 +12,19 @@ AA -> 27
 AB -> 28 
 """
 
+
+def excel_column_to_number(column):
+  # formula = value * (base ** power)
+
+  # declaring var offset = 64
+  # declaring var power = 0
+  # iterate the string from right to left:
+  # value = ord("str") - offset
+  # use formula to find the col_number
+  # power += 1
+  # return final result  
+
+  column_num = ord(column) - 64
+  return column_num
+
+
