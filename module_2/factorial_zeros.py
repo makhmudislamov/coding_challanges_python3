76983fc47d6e72b856bfdb935ef8f03b6d2f83a0
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-10-19 20:13:54 -0700

    solved

--- module_2/factorial_zeros.py
+++ module_2/factorial_zeros.py
@@ -12,4 +12,12 @@ https://docs.google.com/spreadsheets/d/1C6ln6zfmG5Q92fHJosjZxe0WuSS0x_WThzBtc5_n
 
 
 def fast_trailing_zero_factorial(n):
-  return n
+  # The pattern shows that trailin zero amount is equal to how many times n can be divided by 5.
+#   base case
+  if n < 5:
+    return 0
+  else:
+    trailing_num = n // 5
+  return trailing_num
+
+print(fast_trailing_zero_factorial(23))
