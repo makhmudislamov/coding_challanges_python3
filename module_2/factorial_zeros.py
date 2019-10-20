1cff6749f55b95a30b06a01ee08ef28842025099
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-10-19 20:06:11 -0700

    started

---
+++ module_2/factorial_zeros.py
@@ -0,0 +1,15 @@
+"""
+Prompt
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+Your solution should be in logarithmic time complexity.
+
+For a hint, try looking at this spreadsheet, to see if you notice a pattern that can help you predict the number of trailing zeros. What happens as n rises? What are the transition points where the number of trailing zeros rises?
+ 
+https://docs.google.com/spreadsheets/d/1C6ln6zfmG5Q92fHJosjZxe0WuSS0x_WThzBtc5_n6GA/edit#gid=0
+"""
+
+
+def fast_trailing_zero_factorial(n):
+  return n
