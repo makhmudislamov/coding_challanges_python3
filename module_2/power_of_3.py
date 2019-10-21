df18cbfd3b7875116b0511b225152ac66fafe6f4
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-10-21 01:31:08 -0700

    first version

--- module_2/power_of_3.py
+++ module_2/power_of_3.py
@@ -4,3 +4,17 @@ Example:
 Input: 27
 Output: true
 """
+import math
+
+def power_of_three(n):
+    # getting log3(n)
+    log_base_3 = math.log(n, 3)
+    print(log_base_3)
+    print(f"HERE {3 ** 4}")
+    # check if log_base_3 is a whole num
+    if log_base_3 % 2 == 0 or (log_base_3 + 1) % 2 == 0:
+        return True
+    return False
+
+    # return False
+print(power_of_three(45))
