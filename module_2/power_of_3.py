160931f436c74cff33bf0288833f8ba95ca5e2cf
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-10-21 15:26:05 -0700

    rounding the log

--- module_2/power_of_3.py
+++ module_2/power_of_3.py
@@ -7,14 +7,15 @@ Output: true
 import math
 
 def power_of_three(n):
+    # math.pow(3, y)?
     # getting log3(n)
-    log_base_3 = math.log(n, 3)
-    print(log_base_3)
-    print(f"HERE {3 ** 4}")
+    log_base_3 = round(math.log(n, 3))
+    print(f" log3 = {log_base_3}")
+    print(f"HERE {3 ** 5}")
     # check if log_base_3 is a whole num
     if log_base_3 % 2 == 0 or (log_base_3 + 1) % 2 == 0:
         return True
     return False
 
     # return False
-print(power_of_three(45))
+print(power_of_three(243))
