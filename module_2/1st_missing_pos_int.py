3803acf3f90fdc7f716ff9d228da608c8feba7fb
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-10-23 13:08:06 -0700

    translated the pseudocode

--- module_2/1st_missing_pos_int.py
+++ module_2/1st_missing_pos_int.py
@@ -12,6 +12,7 @@ Your algorithm should run in O(n) time.
 def first_missing_positive_integer(integers):
   
 # INPUT >> unsorted array. OUTPUT >> positive int
+
 # PSEUDOCODE
 # create an empty list
 # for every num in the input list
@@ -19,11 +20,24 @@ def first_missing_positive_integer(integers):
     # if the num is larger than the current len of the list, resize the list to be of that length
     # set the value of the place in the list corresponding with that integer to True
 
-    pass
+    found_int = []
+
+    for integer in integers:
+        if integer < 0 :
+            continue
+        if integer + 1 > len(found_int):
+            extend_size = integer - len(found_int) + 1
+            found_int.extend([False] * extend_size)
+        found_int[integer] = True
+
+    return found_int
+
+    
+
 
 
     
 
 
-integers = [-3, -5, 6, 5, 3, 0]
+integers = [1,2,4]
 print(first_missing_positive_integer(integers))
