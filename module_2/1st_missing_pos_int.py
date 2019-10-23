04c872e9ede363d8d20329ad55b80ce1a6ca7312
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-10-23 12:19:06 -0700

    brute force pseudocode

--- module_2/1st_missing_pos_int.py
+++ module_2/1st_missing_pos_int.py
@@ -10,4 +10,13 @@ Your algorithm should run in O(n) time.
 
 
 def first_missing_positive_integer(integers):
-  pass
+  
+# INPUT >> unsorted array. OUTPUT >> positive int
+# PSEUDOCODE
+# iterate ovver the input list
+# deleted all negative numbers
+# create another list thats length is equal to resixed input list - use range()
+# turn both into SET
+# find out which one doesnt exist in input array set
+
+
