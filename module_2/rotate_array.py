a6ee1c66a27f7071e54c91e2478cbc90fd3eb1d9
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-10-26 15:57:41 -0700

    second working solution

--- module_2/rotate_array.py
+++ module_2/rotate_array.py
@@ -27,18 +27,27 @@ def rotate_array(input_array, k):
     # take end of the list item
     # put it to the beginning
 
-    
-    for t in range(1, k + 1):
-        # getting last element
-        end_element = input_array[-1]
-        # inserting the last elemnt to the start of the array
-        input_array.insert(0, end_element)
-        # deleting the lat element
-        input_array.pop()
+    # SOLUTION 1:
+    # for t in range(1, k + 1):
+    #     # getting last element
+    #     end_element = input_array[-1]
+    #     # inserting the last elemnt to the start of the array
+    #     input_array.insert(0, end_element)
+    #     # deleting the lat element
+    #     input_array.pop()
+
+    # SOLUTION 2:
+    # slice the array from last to k index
+    # cut it and past to the beginning of the array
+
+    prepend_elements = input_array[-k:]
+    input_array = prepend_elements + input_array
+    # deleting last k elements
+    del input_array[-k:]
 
     return input_array
 
-input_array = [4, 2, 1, 5, 6]
-k = 3
-print(rotate_array(input_array, 3))
+
+input_array = [1, 2, 3, 4, 5, 6, 7]
+print(rotate_array(input_array, 1))
 
