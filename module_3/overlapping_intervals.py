348aff9097974b10910bf2078bf66effa09127f3
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-23 21:12:07 -0800

    debugged version

--- module_3/overlapping_intervals.py
+++ module_3/overlapping_intervals.py
@@ -12,23 +12,26 @@ Make sure the returned intervals are sorted.
 def merge_overlapping_intervals(intervals):
     # if the input is empty
     # return the input
-    if len(intervals) == 0:
+    if len(intervals) < 2:
         return intervals
 
     index = 0
     while index < len(intervals) - 1:
     #   check if the last item of an array is greater or equal to the first item of next array
         if intervals[index][1] > intervals[index + 1][0]:
+            # deleting items
             del intervals[index][1]
             del intervals[index + 1][0]
+            # merging
             intervals[index].extend(intervals[index + 1])
             del intervals[index + 1]
-        # else:
-        #     return intervals
-        return intervals
-    # else return the input itself
-    # pass
+        else:
+            index += 1
+    return intervals
+ 
 
 
-intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+# intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+intervals = [[1, 3], [2, 6], [15, 18], [16, 20]]
+# [[1, 6], [15, 20]]
 print(merge_overlapping_intervals(intervals))
