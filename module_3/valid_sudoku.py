226a7c8f5f07bdf1855b79d0cbd95d8496b65e7a
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-24 12:05:48 -0800

    trying to use while loop

--- module_3/valid_sudoku.py
+++ module_3/valid_sudoku.py
@@ -25,23 +25,60 @@ Output: true
 
 def validate_sudoku_board(board):
     valid_items = ['.', 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
     # iterate through each nested list
-    for row in board:
-        print(f"this is row {row}")
-        for box in row:
-            print(f"this is box {box}")
-            if valid_items.count(box) >= 1 and box not in valid_items:
-                return False
+    # for row in board:
+    #     print(f"this is row {row}")
+    #     for box in row:
+   
+    #         print(f"this is box {box}")
+    #         # this is horizontal check
+    #         if valid_items.count(box) >= 1 and box not in valid_items:
+    #             print('here')
+    #             return False
+
+    #         elif valid_items.count(box) >= 1 and board[i][j] not in valid_items:
+    #             print('here2', board[i][j])
+    #             return False
+        
+    # return True
+    column_indx = 0
+    row_indx = 0
+    while column_indx < 9 and row_indx < 9:
+        column_item = board[column_indx][row_indx]
+        print(column_item)
+        if valid_items.count(column_item) > 1 and column_item not in valid_items:
+            # print(column_item)
+            
+            return False
+        column_indx += 1
+        if column_indx == 8:
+            row_indx += 1
         
     return True
-    # check if the item is within 1-9 and occured only once
-    # if so return true
-    # else return false
+        # iterate through column first
+        
+        # row_indx += 1
+        # column_indx += 1
+        
     
 
+# should return True
+# board = [
+#             ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+#             ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+#             [".", "9", "8", ".", ".", ".", ".", "6", "."],
+#             ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+#             ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+#             ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+#             [".", "6", ".", ".", ".", ".", "2", "8", "."],
+#             [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+#             [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+#         ]
 
+# should return False
 board = [
-            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["8", "3", ".", ".", "7", ".", ".", ".", "."],
             ["6", ".", ".", "1", "9", "5", ".", ".", "."],
             [".", "9", "8", ".", ".", ".", ".", "6", "."],
             ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
