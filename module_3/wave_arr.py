2cb10690aa5896d5e61e9663aa88f45ec4191a65
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-24 06:55:06 -0800

    started new exercise

---
+++ module_3/wave_arr.py
@@ -0,0 +1,14 @@
+"""
+Given an array of integers, sort the array into a wave like array and return it, 
+In other words, arrange the elements into a sequence such that a[1] >= a[2] <= a[3] >= a[4] <= a[5] ...
+
+Example
+Given [1, 2, 3, 4]
+
+One possible answer : [2, 1, 4, 3]
+Another possible answer : [4, 1, 3, 2]
+"""
+
+
+def wave_array(integers):
+  pass
