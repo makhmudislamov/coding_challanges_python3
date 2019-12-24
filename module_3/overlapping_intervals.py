d09f1a0f6ae59b047e813387a4dc3daeb3ebf7e5
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-23 20:49:16 -0800

    pseudocode

--- module_3/overlapping_intervals.py
+++ module_3/overlapping_intervals.py
@@ -10,5 +10,15 @@ Make sure the returned intervals are sorted.
 """
 
 def merge_overlapping_intervals(intervals):
+    # if the input is empty
+    # return the input
   for interval in intervals:
+    #   check if the last item of an array is greater or equal to the first item of next array
+    #   if so pop those items and merge the arrays
+    #   return the array
+    # else return the input itself
     pass
+
+
+intervals = [1, 3], [2, 6], [8, 10], [15, 18]
+merge_overlapping_intervals(intervals)
