03e74deeb9ad42cf19e95a2ab68b31daf79d0fbc
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-24 11:09:21 -0800

    first attempt - returning False - not working

--- module_3/valid_sudoku.py
+++ module_3/valid_sudoku.py
@@ -24,4 +24,30 @@ Output: true
 
 
 def validate_sudoku_board(board):
-  pass
+    valid_items = [".", 1,2,3,4,5,6,7,8,9]
+    # iterate through each nested list
+    for row in board:
+        for box in row:
+            if box in valid_items and box.count() == 1:
+                return True
+            else:
+                return False
+    # check if the item is within 1-9 and occured only once
+    # if so return true
+    # else return false
+    
+
+
+board = [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+        ]
+        
+print(validate_sudoku_board(board))
