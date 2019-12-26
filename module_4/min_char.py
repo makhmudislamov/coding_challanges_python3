bcf88894640fc050588be88e0199a525a0f7db8e
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-25 21:19:59 -0800

    first attempt works in this test case

--- module_4/min_char.py
+++ module_4/min_char.py
@@ -10,5 +10,25 @@ Output: 2
 Input: AACECAAAA
 Output: 2
 """
-def min_chars(str):
-    pass
+def minimumCharacters(string):
+    # base case the input is already a palindrome
+    # check if reversed is equal to original str
+    # return 0
+    # set counter to zero
+    # while reversed string is not equal to original string:
+    # insert last string to the beginning
+    # incement the counter
+    instered_str = 0
+    last_char = len(string)-1
+    string = list(string)[::-1]
+    while string != string[::-1]:
+        while last_char >= 0:
+            prepended = string[last_char] # gets the last char
+            last_char -= 1
+        string.append(prepended)
+        instered_str += 1
+    return instered_str
+    # return list(string)[::-1]
+
+string = "ACECAAAAAAA"
+print(minimumCharacters(string))
