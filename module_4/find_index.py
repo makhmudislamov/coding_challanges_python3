f26b7b3d7f38f6d57a46531c1a3f7f1f14894e78
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-28 14:38:50 -0800

    passed all tests

--- module_4/find_index.py
+++ module_4/find_index.py
@@ -19,20 +19,22 @@ def find_index(sorted_list, target):
     # return its index
     # else
     # return the index of element that is target-1 
-    if not sorted_list:
+    if not sorted_list or target < sorted_list[0]:
         return 0
     
     
     for num in sorted_list:
         index = sorted_list.index(num)
-        print(num)
+        
         if num == target:
             return sorted_list.index(num)
-        elif sorted_list[index] <= target <= sorted_list[index + 1]:
+        elif sorted_list[index] < target < sorted_list[index + 1]:
             return index + 1
+        elif target > sorted_list[len(sorted_list)-1]:
+            return len(sorted_list)
 
 
-sorted_list = [1, 3, 5, 6]
-target = 7
+sorted_list = [1, 3, 5, 9]
+target = 3
 
 print(find_index(sorted_list, target))
