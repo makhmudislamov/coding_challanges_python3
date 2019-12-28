d257acdc615c94e98d7624002b1e3a83bcc960f8
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-27 18:58:31 -0800

    started this exercise

--- module_4/longest_palindromic.py
+++ module_4/longest_palindromic.py
@@ -24,36 +24,61 @@ def longest_palindrome(input_string):
     # keep removing char from the end and beginning of the string
     # after each removal check if current string is palindromic
     # if so return it
+
+    # OPTIMAL:
+    # wrap the input into list
+    # and check if any string is a pal.
+    # return the longest
+    # else continue
+    
     input_string = input_string.split(' ')
     print(input_string)
-    palindromes = []
-    for string in input_string:
-        print(string)
-        if len(string) > 1 and string == string[::-1]:
-            palindromes.append(string)
-        else:
-            continue
+    palindromes = ['h']
+    index = 0
 
-    input_string = ' '.join(input_string)
-    print("here", input_string)
-    start = 1
-    end = len(input_string)-2
-    while input_string != input_string[::-1] or len(input_string) != 0:
+    while index < len(input_string) - 1:
+        if len(input_string[index]) > len(palindromes[0]) and input_string[index] == input_string[index][::-1]:
+            palindromes.pop()
+            palindromes.append(input_string[index])
+            print("pals", palindromes)    
+            index += 1
+        # else:
+        #     index += 1 
 
-        input_string =  input_string[start::]
-        # print("deleted start", input_string)
-        if input_string != input_string[::-1]:
-            # print('not yet')
-            input_string = input_string[:end:]
-            # print("del end", input_string)
-        if input_string == input_string[::-1]:
-
-            return input_string
-        else:        
-            end -= 1
+    return palindromes[0]
 
 
-    return input_string
+    # for string in input_string:
+    #     print(string)
+    #     if len(string) > len(palindromes[0]) and string == string[::-1]:
+    #         palindromes.pop()
+    #         palindromes.append(string)
+    #         print("pals", palindromes)
+    #         return palindromes[0]      
+    #     else:
+    #         continue
+        
+
+    # input_string = ' '.join(input_string)
+    # print("here", input_string)
+    # start = 1
+    # end = len(input_string)-2
+    # while input_string != input_string[::-1] or len(input_string) != 0:
+
+    #     input_string =  input_string[start::]
+    #     # print("deleted start", input_string)
+    #     if input_string != input_string[::-1]:
+    #         # print('not yet')
+    #         input_string = input_string[:end:]
+    #         # print("del end", input_string)
+    #     if input_string == input_string[::-1]:
+
+    #         return input_string
+    #     else:        
+    #         end -= 1
+
+
+    # return input_string
 
 
 # input_string = "babad"
