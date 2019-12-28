3a2dd2d9208985eaa98f38580739feff28564761
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-28 14:18:57 -0800

    first attempt

--- module_4/find_index.py
+++ module_4/find_index.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Given a sorted array and a target value, return the index if the target is found. 
 If not, return the index where it would be if it were inserted in order.
@@ -9,8 +10,8 @@ Examples:
 [1,3,5,6], 2 → 1
 [1,3,5,6], 7 → 4
 [1,3,5,6], 0 → 0
-"""
 
+"""
 
 def find_index(sorted_list, target):
     # iterate over the input
@@ -18,4 +19,20 @@ def find_index(sorted_list, target):
     # return its index
     # else
     # return the index of element that is target-1 
-    pass
+    if not sorted_list:
+        return 0
+    
+    
+    for num in sorted_list:
+        
+        print(num)
+        if num == target:
+            return sorted_list.index(num)
+        else:
+            return sorted_list.index(target-1)
+
+
+sorted_list = [1, 3, 5, 6]
+target = 5
+
+print(find_index(sorted_list, target))
