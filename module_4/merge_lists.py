02461eb2bd0dc23dfc15bee295b646406a653852
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-28 15:55:03 -0800

    merged the list different way

--- module_4/merge_lists.py
+++ module_4/merge_lists.py
@@ -22,18 +22,30 @@ def merge_sorted_lists(nums1, nums2):
     # if element is out of order swap the elements
     # return the modified list
 
-    nums1.extend(nums2)
-    print(nums1)
+    # replacing last n elements of nums1 with nums2 elements.
+    
+    # nums1.extend(nums2)
+
+    nums1_index = len(nums1) - 1
+    nums2_index = len(nums2) - 1
+
+    while nums2_index >= 0:
+        nums1[nums1_index] = nums2[nums2_index]
+        nums1_index -= 1
+        nums2_index -= 1
+
+    print("nums1 after replace", nums1)
+
     index = 0
     while index < len(nums1) - 1:
         if nums1[index] > nums1[index + 1]:
-            print(f"sorting {nums1}")
+        
             nums1[index], nums1[index + 1] = nums1[index + 1], nums1[index]
         index += 1
 
     return nums1
 
 
-nums1 = [1, 2, 3]
+nums1 = [1, 2, 3, 0, 0, 0]
 nums2 = [2, 5, 6]
 print(merge_sorted_lists(nums1, nums2))
