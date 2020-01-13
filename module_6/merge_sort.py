9f11c59d76817090f4e09bef384d80bdf15d6c0e
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-01-13 08:48:58 -0800

    pseudocode

--- module_6/merge_sort.py
+++ module_6/merge_sort.py
@@ -6,4 +6,23 @@ Don't use the built-in sorted or list.sort() methods - the goal of this is to un
 
 
 def merge_sort(nums):
-  pass
+    if len(nums) > 1:
+        # bisect the list
+        # create two lists, for left and right halves
+        # sort them
+        right = merge_sort(right)
+        left = merge_sort(left)
+
+        sorted_list = []
+        # merge the sorted list
+        # until at least one of them is empty:
+            # check the first elements of both lists:
+            # append the smaller one to sorted
+        
+        sorted_list.extend(right)
+        sorted_list.extend(left)
+
+        return sorted_list
+    else:
+        return nums
+    
