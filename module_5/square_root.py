579d4c3072534549cb983018bff49731c6b5cf0b
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-01-12 19:49:41 -0800

    brute force pseudocode

--- module_5/square_root.py
+++ module_5/square_root.py
@@ -1,28 +1,35 @@
 """
 Implement int sqrt(int x).
-
 Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
-
 Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
-
 Do not use any built square root functions.
 
 Example 1:
 Input: 4
 Output: 2
 
-
 Example 2:
 Input: 8
 Output: 2
+
 Explanation: The square root of 8 is 2.82842..., and since 
 the decimal part is truncated, 2 is returned.
 """
 
 
-def squareRoot(num):
-    return
+def square_root(num):
+#   log (base2) num = ?
+#   log(base2) 9 = X
+#   X ** 2 = 9
+
+    # declare variable that is equal to 0
+    # until the variable is not equal to num
+    # variable ** 2
+    # increment the variable
+
+    # return
+    pass
 
 
 sampleInput = 9
-print(squareRoot(sampleInput))
+print(square_root(sampleInput))
