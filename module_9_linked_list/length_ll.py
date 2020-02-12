737337a8f27c519506a2893eb14fa77ae534f708
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-12 09:53:55 -0800

    started new module for linkedlist

---
+++ module_9_linked_list/length_ll.py
@@ -0,0 +1,46 @@
+"""
+Write a method for a Linked List that returns the length of the linked list.
+
+For example:
+
+ll = LinkedList()
+ll.extend([1, 2, 3, 4, 5])
+
+ll.length() # returns 5
+"""
+
+
+class LinkedNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+### do not modify this class, or any of the methods in it, other than length()
+### you may insert new methods if you like
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def empty(self):
+        return self.head == None
+
+    def append(self, data):
+        if self.empty():
+            self.head = LinkedNode(data)
+            self.tail = self.head
+        else:
+            new_node = LinkedNode(data)
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def extend(self, array):
+        for element in array:
+            self.append(element)
+
+  # implement this method
+  # return the length of the linked list, an integer value
+    def length(self):
+        return -1
