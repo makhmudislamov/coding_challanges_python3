d0bd306570324b4641944050810d7bae028f7c60
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-17 11:36:43 -0800

    added model solution

--- module_12_trees/traversal.py
+++ module_12_trees/traversal.py
@@ -105,3 +105,72 @@ tree.root.left.left = Node(3)
 tree.root.left.right = Node(7)
 
 print(tree.post_order_traversal())
+
+# MODEL SOLUTION
+# class Node:
+#   def __init__(self, data):
+#     self.data = data
+#     self.left = None
+#     self.right = None
+
+
+# class Tree:
+#   def __init__(self):
+#     self.root = None
+
+#   def level_order_traversal(self):
+#     # return the tree as a list in a level-order sequence
+#     traversed_list = []
+#     if not self.root:
+#       return []
+
+#     queue = [self.root]
+
+#     while len(queue) > 0:
+#       current_node = queue.pop(0)
+#       traversed_list.append(current_node.data)
+#       if current_node.left:
+#         queue.append(current_node.left)
+#       if current_node.right:
+#         queue.append(current_node.right)
+
+#     return traversed_list
+
+#   def pre_order_traversal(self):
+#     # return the tree as a list in a pre-order sequence (dfs)
+#     traversed_list = []
+
+#     def dfs(node, traversed_list):
+#       if node:
+#         traversed_list.append(node.data)
+#         dfs(node.left, traversed_list)
+#         dfs(node.right, traversed_list)
+
+#     dfs(self.root, traversed_list)
+#     return traversed_list
+
+#   def in_order_traversal(self):
+#     # return the tree as a list in-order (dfs)
+#     traversed_list = []
+
+#     def dfs(node, traversed_list):
+#       if node:
+#         dfs(node.left, traversed_list)
+#         traversed_list.append(node.data)
+#         dfs(node.right, traversed_list)
+
+#     dfs(self.root, traversed_list)
+#     return traversed_list
+
+#   def post_order_traversal(self):
+#     # return the tree as a list in a post-order sequence (dfs)
+#     traversed_list = []
+
+#     def dfs(node, traversed_list):
+#       if node:
+#         dfs(node.left, traversed_list)
+#         dfs(node.right, traversed_list)
+#         traversed_list.append(node.data)
+
+#     dfs(self.root, traversed_list)
+#     return traversed_list
